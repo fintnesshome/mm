7b964f887ac016f462eca9a0fd9f9b0f38257b20
fintnesshome <133360855+fintnesshome@users.noreply.github.com>
2023-05-20 14:59:53 +0300

    Add files via upload

---
+++ mastan/index.php
@@ -0,0 +1,48 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+if(!empty($_POST["name"])&&!empty($_POST["emaile"])){
+
+      echo "youre name".$_POST["name"]."<br>" ;
+
+       echo "youre maile".$_POST["emaile"] ;
+    
+}
+else{
+
+    echo "empty name and emaile";
+}
+
+
+
+
+
+
+
+
+
+  ?>
+
+    </body>
+
+</html>
+
+
+
+
+
+
+</body>
+</html>
+
